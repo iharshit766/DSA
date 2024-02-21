7acf8885e3ce4d7876adad2cd3c44a8630e774c4
Harshit Kumar <155983218+iharshit766@users.noreply.github.com>
2024-02-21 13:26:11 +0530

    Add files via upload

---
+++ Strings/character.java
@@ -0,0 +1,13 @@
+package Strings;
+import java.util.*;
+public class character {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String st="Hello";
+		String a="Hello";
+		System.out.println(st);
+	}
+	
+}
